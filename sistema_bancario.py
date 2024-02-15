504c566657e964ecdfc25729ac39b04754a3eefe
Karen Gabriella <79243574+karengabriella@users.noreply.github.com>
2024-02-14 22:46:44 -0300

    Sistema Bancário Simples
    #Operacoes Bancárias
    
    # 1 - Deposito
    # 2 - Saque
    # 3-  Extrato
    # 4 - Sair
    
    #REGRAS: 
     #Limite por saque de 500
     #Limite diario de 3 saques
     #Extrato de valor sacado

---
+++ sistema_bancario.py
@@ -0,0 +1,40 @@
+def deposito(saldo):
+  valor = int(input("Qual valor irá depositar?  "))
+  if(valor <= 0):
+    print("Valor inválido, deposite um valor positivo.")
+  else:
+    saldo = saldo + valor
+    print("Depósito realizado com sucesso.")
+    print("Saldo atual: ", saldo)
+    return saldo
+
+
+
+def saque(saldo, limite_diario, extrato):
+  valor = int(input("Qual valor irá sacar?  "))
+  
+  if(limite_diario >= 3):
+    print("Limite diário excedido.")
+    return saldo, limite_diario, extrato
+  else:
+    
+    if(valor > 500 or valor > saldo):
+      print("Valor inválido, saque não pode ser maior que o saldo ou maior que 500.")
+      print("Limite diário: ", limite_diario)
+      print("Saldo: ", saldo)
+      return saldo, limite_diario, extrato
+
+    else:
+      saldo = saldo - valor
+      extrato = extrato + valor
+      limite_diario += 1
+      print("Saque realizado com sucesso.")
+      print("Limite diário: ", limite_diario)
+      print("Saldo: ", saldo)
+
+      return saldo, limite_diario, extrato
+    
+
+def consulta_extrato(extrato):
+  print("Extrato: ", extrato)
+  return extrato
